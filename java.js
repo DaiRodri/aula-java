0c8922e220b86cfdb36fdb13d19360b2217e0e4b
DaiRodri <daianarodri@gmail.com>
2024-08-30 01:54:21 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,5 @@
+console.log("Olá, Mundo!");
+let preco = prompt (" digite um preço");
+document.write("o preço é:",preco,"<br>","<br>");
+let nome= prompt(" Digite o seu nome");
+document.write("Olá:" ,nome," . Seja bem Vindo ");
